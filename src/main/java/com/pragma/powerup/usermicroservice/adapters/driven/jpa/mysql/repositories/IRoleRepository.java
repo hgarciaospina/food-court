b1d9bee2763047d8826a838c0388388800771f9a
Henry García <henrygarciaospina@gmail.com>
2023-05-06 18:36:51 -0500

    key encryption when creating use

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/repositories/IRoleRepository.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driven/jpa/mysql/repositories/IRoleRepository.java
@@ -1,6 +1,9 @@
 package com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.repositories;
 
 import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.entity.RoleEntity;
+import com.pragma.powerup.usermicroservice.domain.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IRoleRepository extends JpaRepository<RoleEntity, Long> {}
+public interface IRoleRepository extends JpaRepository<RoleEntity, Long> {
+    Role findByName(String name);
+}
--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/handlers/impl/UserHandlerImpl.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/handlers/impl/UserHandlerImpl.java
@@ -1,20 +1,32 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.handlers.impl;
 
+import com.pragma.powerup.usermicroservice.adapters.driven.jpa.mysql.repositories.IRoleRepository;
 import com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request.UserRequestDto;
 import com.pragma.powerup.usermicroservice.adapters.driving.http.handlers.IUserHandler;
 import com.pragma.powerup.usermicroservice.adapters.driving.http.mapper.IUserRequestMapper;
 import com.pragma.powerup.usermicroservice.domain.api.IUserServicePort;
+import com.pragma.powerup.usermicroservice.domain.model.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserHandlerImpl implements IUserHandler {
     private final IUserServicePort personServicePort;
+    private final IRoleRepository personRole;
     private final IUserRequestMapper personRequestMapper;
 
     @Override
     public void saveUser(UserRequestDto userRequestDto) {
+        Role _role = personRole.findByName("ROLE_ADMIN");
+        userRequestDto.setIdRole(_role.getId());
+        encryptPasswordUserRequestDto(userRequestDto);
         personServicePort.saveUser(personRequestMapper.toUser(userRequestDto));
+    }
+    public void encryptPasswordUserRequestDto (UserRequestDto userRequestDto) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String password = userRequestDto.getPassword();
+        userRequestDto.setPassword(passwordEncoder.encode(password));
     }
 }
