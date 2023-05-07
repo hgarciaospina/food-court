0d2857a4bdbbea0cd0ea2443847f8877935d8ef7
Henry García <henrygarciaospina@gmail.com>
2023-05-06 21:27:09 -0500

    New changes in UserResponseDto

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/response/UserResponseDto.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/response/UserResponseDto.java
@@ -3,12 +3,16 @@ package com.pragma.powerup.usermicroservice.adapters.driving.http.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @Getter
 public class UserResponseDto {
     private String name;
     private String surname;
-    private String email;
+    private Long dniNumber;
+    private LocalDate birthdate;
     private String phone;
-    private Long  idRole;
+    private String email;
+    private Long idRole;
 }
